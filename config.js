e5971add2c547e256693614f4dcaeae5bb182696
Nora <107586461+noora-bot@users.noreply.github.com>
2025-01-21 03:16:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const ownervb = process.env.OWNERS || "254743399594;917605902011;GURU;6281255369012;Ash"
+const ownervb = process.env.OWNERS || "966541908619;917605902011;GURU;6281255369012;Ash"
 
 
 const ownerlist = ownervb.split(';');
@@ -23,9 +23,9 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['254743399594', '917605902011', '32460248586']
-global.prems = ['254743399594', '917605902011', '32460248586', '919398758484']
-global.allowed = ['254743399594', '917605902011', '32460248586', '19152999993']
+global.mods = ['966541908619', '917605902011', '32460248586']
+global.prems = ['966541908619', '917605902011', '32460248586', '919398758484']
+global.allowed = ['966541908619', '917605902011', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
